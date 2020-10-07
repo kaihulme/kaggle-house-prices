efbdbbb587f151ec61d2185e978e624ce049018b
kaihulme <kaihulme97@gmail.com>
2020-10-07 20:22:10 +0100

    Initial data exploration

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Submission for House Prices: Advanced Regression Techniques Kaggle competition.',
+    author='kaihulme',
+    license='',
+)
